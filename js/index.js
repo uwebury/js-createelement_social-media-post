457d40c840499b4b430a9ed9a8b17e76d6f03fb9
uwebury <142432719+uwebury@users.noreply.github.com>
2023-11-22 14:08:05 +0100

    Social media post replicated

--- js/index.js
+++ js/index.js
@@ -10,3 +10,31 @@ likeButton.addEventListener("click", handleLikeButtonClick);
 
 // Exercise:
 // Use document.createElement() and append another social media post to the body.
+
+const post = document.createElement("article");
+post.classList.add("post");
+
+const postContent = document.createElement("p");
+postContent.classList.add("post__content");
+postContent.textContent =
+  " Lorem ipsum dolor, sit amet consectetur adipisicing elit.";
+
+const postFooter = document.createElement("footer");
+postFooter.classList.add("post__footer");
+
+const postUserName = document.createElement("span");
+postUserName.classList.add("post__username");
+postUserName.textContent = "@username";
+
+const postButton = document.createElement("button");
+postButton.type = "button";
+postButton.classList.add("post__button");
+postButton.textContent = " ♥ Like";
+//postButton.setAttribute("data-brokkoli", "like-button");
+postButton.addEventListener("click", handleLikeButtonClick);
+
+postFooter.append(postUserName, postButton);
+
+post.append(postContent, postFooter);
+
+document.body.append(post);
